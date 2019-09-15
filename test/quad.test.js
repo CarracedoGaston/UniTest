0b3776d8989d3d2cfe9d4423d465f2da6709679a
Carracedo Gaston <carracedo.gaston@gmail.com>
2019-09-15 12:15:40 -0300

    Quad: getHeight

--- test/quad.test.js
+++ test/quad.test.js
@@ -63,4 +63,64 @@ describe('Quad', () => {
 
   })
 
+  describe('getHeight', () => {
+    
+    it('Should return 5 && 0 when created with a height of 5 and a undefined width', () => {
+      const data = new Quad(5, undefined)
+      const result = [data.height, data.width]
+      const expected = [5, 0]
+      assert.deepEqual(result, expected)
+      })
+  
+    it('Should return 6.27 && 0 when created with a height of 6.27 and a undefined width', () => {
+      const data = new Quad(6.27, undefined)
+      const result = [data.height, data.width]
+      const expected = [6.27, 0]
+      assert.deepEqual(result, expected)
+    })
+
+    it('Should return 0 && 0 when created with a height of -1 and a undefined width', () => {
+      const data = new Quad(-1, undefined)
+      const result = [data.height, data.width]
+      const expected = [0, 0]
+      assert.deepEqual(result, expected)
+    })
+  
+    it('Should return 0 && 0 when created with a height of a character and a undefined width', () => {
+      const data = new Quad('a', undefined)
+      const result = [data.height, data.width]
+      const expected = [0, 0]
+      assert.deepEqual(result, expected)
+    })
+
+    it('Should return 0 && 0 when created with a height of an array and a undefined width', () => {
+      const data = new Quad([], undefined)
+      const result = [data.height, data.width]
+      const expected = [0, 0]
+      assert.deepEqual(result, expected)
+    })
+  
+    it('Should return 0 && 0 when created with a height of an object and a undefined width', () => {
+      const data = new Quad({}, undefined)
+      const result = [data.height, data.width]
+      const expected = [0, 0]
+      assert.deepEqual(result, expected)
+    })
+  
+    it('Should return 0 && 0 when created with a height of a true valor and a undefined width', () => {
+      const data = new Quad(true, undefined)
+      const result = [data.height, data.width]
+      const expected = [0, 0]
+      assert.deepEqual(result, expected)
+    })
+  
+    it('Should return 0 && 0 when created with a height of false valor and a undefined width', () => {
+      const data = new Quad(false, undefined)
+      const result = [data.height, data.width]
+      const expected = [0, 0]
+      assert.deepEqual(result, expected)
+    })
+
+  })
+
 })
